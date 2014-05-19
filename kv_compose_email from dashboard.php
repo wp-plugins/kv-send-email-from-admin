a3160a637d939bb7137e589c6d2a02e6f62634d7
kvvaradha <kvvaradha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-19 05:21:36 +0000

    compose email
    git-svn-id: https://plugins.svn.wordpress.org/kv-send-email-from-admin/trunk@916923 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kv_compose_email from dashboard.php
+++ kv_compose_email from dashboard.php
@@ -38,24 +38,22 @@ function kv_codes_plugins() {
 
 ?>
  <div class="wrap">
-        <div class="icon32" id="icon-tools"><br/></div>
-        <h2><?php _e('KvCodes', 'kvcodes') ?></h2>		
-		<div class="welcome-panel">
-								<?php //kv_admin_thirty_day_chart () ; ?>
-								Thank you for using Kvcodes Plugins . Here is my few Plugins work .MY plugins are very light weight and Simple.  <p>
-								<a href="http://www.kvcodes.com/" target="_blank" ><h3> Visit My Blog</h3></a></p> 
-								</div> 
-		<div id="poststuff" > 
-			<div id="post-body" class="metabox-holder columns-2" >
-				<div id="post-body-content" > 
-					<div class="meta-box-sortables"> 
-						<div id="dashboard_right_now" class="postbox">
-							<div class="handlediv" > <br> </div>
-							<h3 class="hndle"  ><img src="<?php echo KV_COMPOSE_EMAIL_URL.'/images/kv_logo.png'; ?>" >  My plugins </h3> 
-							<div class="inside" style="padding: 10px; "> 
-								
-								<?php 
-									$kv_wp =  kv_get_web_page('http://profiles.wordpress.org/kvvaradha'); 
+    <div class="icon32" id="icon-tools"><br/></div>
+    <h2><?php _e('KvCodes', 'kvcodes') ?></h2>		
+	<div class="welcome-panel">
+		Thank you for using Kvcodes Plugins . Here is my few Plugins work .MY plugins are very light weight and Simple.  <p>
+		<a href="http://www.kvcodes.com/" target="_blank" ><h3> Visit My Blog</h3></a></p> 
+	</div> 
+	
+	<div id="poststuff" > 
+		<div id="post-body" class="metabox-holder columns-2" >
+			<div id="post-body-content" > 
+				<div class="meta-box-sortables"> 
+					<div id="dashboard_right_now" class="postbox">
+						<div class="handlediv" > <br> </div>
+						<h3 class="hndle"  ><img src="<?php echo KV_COMPOSE_EMAIL_URL.'/images/kv_logo.png'; ?>" >  My plugins </h3> 
+						<div class="inside" style="padding: 10px; "> 								
+							<?php $kv_wp =  kv_get_web_page('http://profiles.wordpress.org/kvvaradha'); 
 									
 									 $kv_first_pos = strpos($kv_wp['content'], '<div id="content-plugins" class="info-group plugin-theme main-plugins inactive">');
 									
@@ -65,47 +63,41 @@ function kv_codes_plugins() {
 									
 									$kv_sec_trim = substr($kv_first_trim ,0, $kv_sec_pos );  
 									
-									echo $kv_sec_trim; 					?> 
-								</div>
-								</div>
-							</div>
+									echo $kv_sec_trim; 	?> 
 						</div>
 					</div>
-				</div> 
-				
-				<div id="postbox-container-1" class="postbox-container" > 
-					<div class="meta-box-sortables"> 
-						<div id="postbox-container-2" class="postbox-container" > 
-						
-						<div id="dashboard_right_now" class="postbox">
-							<div class="handlediv" > <br> </div>
-							<h3 class="hndle" ><img src="<?php echo KV_COMPOSE_EMAIL_URL.'/images/kv_logo.png'; ?>" >  Donate </h3> 
-							<div class="inside" style="padding: 10px; " > 
-							<b>If i helped you, you can buy me a coffee, just press the donation button :)</b> 
-							<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-								<input type="hidden" name="cmd" value="_donations" />
-								<input type="hidden" name="business" value="<?php echo 'kvvaradha@gmail.com'; ?>" />
-								<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
-								<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
-							</form>
-							</div> 
-						</div> 
-						</div>
-						<div id="postbox-container-2" class="postbox-container" > 
-						<div id="dashboard_quick_press" class="postbox">
-							<div class="handlediv" > <br> </div>
-							<h3 class="hndle" ><img src="<?php echo KV_COMPOSE_EMAIL_URL.'/images/kv_logo.png'; ?>" >  Support me from Facebook </h3> 
-							<div class="inside" style="padding: 10px; "> 
-							<p><iframe allowtransparency="true" frameborder="0" scrolling="no" src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fkvcodes&amp;width=180&amp;height=300&amp;show_faces=true&amp;colorscheme=light&amp;stream=false&amp;show_border=false&amp;header=false&amp;appId=117935585037426" style="border:none; overflow:hidden; width:250px; height:300px;"></iframe></p>
-							</div> 
-						</div> 
-						</div>
-					</div>
-				</div> 
-				
+				</div>							
 			</div>
-			</div> 
-
+		</div>
+	</div> 			
+	<div id="postbox-container-1" class="postbox-container" > 
+		<div class="meta-box-sortables"> 
+			<div id="postbox-container-2" class="postbox-container" >
+				<div id="dashboard_right_now" class="postbox">
+					<div class="handlediv" > <br> </div>
+					<h3 class="hndle" ><img src="<?php echo KV_COMPOSE_EMAIL_URL.'/images/kv_logo.png'; ?>" >  Donate </h3> 
+					<div class="inside" style="padding: 10px; " > 
+						<b>If i helped you, you can buy me a coffee, just press the donation button :)</b> 
+						<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+							<input type="hidden" name="cmd" value="_donations" />
+							<input type="hidden" name="business" value="<?php echo 'kvvaradha@gmail.com'; ?>" />
+							<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
+							<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+						</form>
+					</div> 
+				</div> 
+			</div>
+			<div id="postbox-container-2" class="postbox-container" > 
+				<div id="dashboard_quick_press" class="postbox">
+					<div class="handlediv" > <br> </div>
+					<h3 class="hndle" ><img src="<?php echo KV_COMPOSE_EMAIL_URL.'/images/kv_logo.png'; ?>" >  Support me from Facebook </h3> 
+					<div class="inside" style="padding: 10px; "> 
+						<p><iframe allowtransparency="true" frameborder="0" scrolling="no" src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fkvcodes&amp;width=180&amp;height=300&amp;show_faces=true&amp;colorscheme=light&amp;stream=false&amp;show_border=false&amp;header=false&amp;appId=117935585037426" style="border:none; overflow:hidden; width:250px; height:300px;"></iframe></p>
+					</div> 
+				</div> 
+			</div>
+		</div>
+	</div> 				
 </div> <!-- /wrap -->
 <?php
 
